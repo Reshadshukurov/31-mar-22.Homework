da7e0cf566a0584b5f99249710d97622d69ed08c
reshad <you@example.com>
2022-04-01 01:58:37 +0400

    first commit

---
+++ 31-mar-22.Homework/Calculator .cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _31_mar_22.Homework
+{
+   static  class Calculator
+    {
+        public static int Sum( int a,int b)
+        {
+            return a + b;
+        }
+        public static int Subtract(int x, int y)
+        {
+            return x - y;
+        }
+        public static int Multiply(int x, int y)
+        {
+            return x * y;
+        }
+
+        public static int Divide(int x1, int y1)
+        {
+            return x1 / y1;
+        }
+
+    }
+}
---
+++ 31-mar-22.Homework/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _31_mar_22.Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator.Sum(2,4);
+            Calculator.Subtract(5, 3);
+            Calculator.Multiply(6, 4);
+            Calculator.Divide(21, 3);
+            Console.WriteLine(Calculator.Sum(2, 4));
+            Console.WriteLine(Calculator.Subtract(5, 3));
+            Console.WriteLine(Calculator.Multiply(6, 4));
+            Console.WriteLine(Calculator.Divide(21, 3));
+            Console.ReadLine();
+
+        }
+    }
+}
